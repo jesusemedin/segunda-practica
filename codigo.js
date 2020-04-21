f1cc5915690f90b9da71b5c6fc7c818b392223bd
Jesus Medina <medinabotamail@gmail.com>
2020-04-21 11:59:34 -0400

    se agrego 2 alertas

--- codigo.js
+++ codigo.js
@@ -1,4 +1,6 @@
 //alert: Funcion
 // (): Parametros de la funcion
 // "": Cadenas de textos
-alert("Hola mundo, estoy aprendiendo a programar");
+alert("Hola mundo, estoy aprendiendo a programar")
+alert("Sigo probando este archivo")
+alert( 2*2 )
